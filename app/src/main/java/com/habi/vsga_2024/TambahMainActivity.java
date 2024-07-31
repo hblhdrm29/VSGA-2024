e621020c3443299ea6919907d076e66d9734c1d8
Habillah Darma <hdhmmz@gmail.com>
2024-07-31 10:47:32 +0700

    membuat layout

--- app/src/main/java/com/habi/vsga_2024/TambahMainActivity.java
+++ app/src/main/java/com/habi/vsga_2024/TambahMainActivity.java
@@ -1,6 +1,5 @@
 package com.habi.vsga_2024;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-public class MainActivity extends AppCompatActivity {
-
-    private FloatingActionButton fab;
+public class TambahMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tambah_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        fab = findViewById(R.id.floatingActionButton);
-        fab.setOnClickListener(v -> {
-            Intent i = new Intent(MainActivity.this, TambahMainActivity.class);
-            startActivity(i);
         });
     }
 }
