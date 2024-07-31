7330d70041a61f038f3a32fce32a4e1490c766b3
Habillah Darma <hdhmmz@gmail.com>
2024-07-31 11:06:24 +0700

    Membuat list catatan muncul ditampilan utama

--- app/src/main/java/com/habi/vsga_2024/MainAdapter.java
+++ app/src/main/java/com/habi/vsga_2024/MainAdapter.java
@@ -12,29 +12,29 @@ import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
-    public class MainAdapter extends ArrayAdapter<Catatan> {
+public class MainAdapter extends ArrayAdapter<Catatan> {
 
-        public MainAdapter(@NonNull Context context) {
-            super(context, 0, new ArrayList<>());
-        }
-
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            View itemView = convertView;
-            if (itemView == null) {
-                itemView = LayoutInflater.from(getContext()).inflate(
-                        android.R.layout.simple_list_item_2, parent, false);
-            }
-
-            Catatan catatan = getItem(position);
-
-            TextView textView1 = itemView.findViewById(android.R.id.text1);
-            textView1.setText(catatan.getNama());
-
-            TextView textView2 = itemView.findViewById(android.R.id.text2);
-            textView2.setText(catatan.getTimestamp());
-
-            return itemView;
-        }
+    public MainAdapter(@NonNull Context context) {
+        super(context, 0, new ArrayList<>());
     }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View itemView = convertView;
+        if (itemView == null) {
+            itemView = LayoutInflater.from(getContext()).inflate(
+                    android.R.layout.simple_list_item_2, parent, false);
+        }
+
+        Catatan catatan = getItem(position);
+
+        TextView textView1 = itemView.findViewById(android.R.id.text1);
+        textView1.setText(catatan.getNama());
+
+        TextView textView2 = itemView.findViewById(android.R.id.text2);
+        textView2.setText(catatan.getTimestamp());
+
+        return itemView;
+    }
+}
