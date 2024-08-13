e3206b16143a396a7f9cd1b329319148f7f25313
Habillah Darma <hdhmmz@gmail.com>
2024-08-13 08:06:43 +0700

    sensor

--- app/src/main/java/com/habi/vsga_2024/MainActivity.java
+++ app/src/main/java/com/habi/vsga_2024/MainActivity.java
@@ -1,6 +1,13 @@
 package com.habi.vsga_2024;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +15,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager manager;
+    private Sensor mLight;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mLight = manager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        textView = findViewById(R.id.textView);
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float lux = event.values[0];
+        textView.setText(getString(R.string.nilai_sensor_cahaya, lux));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        manager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        manager.unregisterListener(this);
+        super.onPause();
     }
 }
